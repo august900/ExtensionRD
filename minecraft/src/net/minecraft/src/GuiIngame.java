80d3ca32fec58922626590234c78eac90cb8b60f
Steve404_ <stefanpcornea@gmail.com>
2026-07-04 12:48:45 +0300

    VERSION 02

--- minecraft/src/net/minecraft/src/GuiIngame.java
+++ minecraft/src/net/minecraft/src/GuiIngame.java
@@ -140,7 +140,7 @@ public class GuiIngame extends Gui {
 		GL11.glDisable(GL12.GL_RESCALE_NORMAL);
 		String var21;
 		if(Keyboard.isKeyDown(Keyboard.KEY_F3)) {
-			var8.drawStringWithShadow("Minecraft Alpha v1.14.06_01 (" + this.mc.debug + ")", 2, 2, 16777215);
+			var8.drawStringWithShadow("Minecraft Alpha v1.14.06_02 (" + this.mc.debug + ")", 2, 2, 16777215);
 			var8.drawStringWithShadow(this.mc.debugInfoRenders(), 2, 12, 16777215);
 			var8.drawStringWithShadow(this.mc.getEntityDebug(), 2, 22, 16777215);
 			var8.drawStringWithShadow(this.mc.debugInfoEntities(), 2, 32, 16777215);
@@ -154,7 +154,7 @@ public class GuiIngame extends Gui {
 			var21 = "Allocated memory: " + var27 * 100L / var22 + "% (" + var27 / 1024L / 1024L + "MB)";
 			this.drawString(var8, var21, var6 - var8.getStringWidth(var21) - 2, 12, 14737632);
 		} else {
-			var8.drawStringWithShadow("Minecraft Alpha v1.14.06_01", 2, 2, 16777215);
+			var8.drawStringWithShadow("Minecraft Alpha v1.14.06_02", 2, 2, 16777215);
 		}
 
 		if(this.recordPlayingUpFor > 0) {
